4aa987f22f4ecc8f2564b575401b216728390f10
Felipe Rohde <feliperohdee@gmail.com>
2024-08-14 20:51:08 -0300

    bump package name

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'tailwind-colors-generator' {
+declare module '@simpleimg/tailwind-colors-generator' {
 	export interface ClosestColor {
 		closestShadeLightness: {
 			number: number;
